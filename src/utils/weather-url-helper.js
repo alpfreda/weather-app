67c54f5c2d094a8a52c19e1efa855aa9713e96c7
jsgeekes <jsgeekes@gmail.com>
2020-10-26 21:46:57 +0300

    improve weather url helper

--- src/utils/weather-url-helper.js
+++ src/utils/weather-url-helper.js
@@ -1,6 +1,18 @@
-export const getBathCitiesUrl = ({cities}) => {
-  return `${process.env.WEATHER_API_BASE_URL}/${process.env.WEATHER_API_SEVERAL_CITIES.replace(
-    '{cities}',
-    cities,
-  )}&appid=${process.env.WEATHER_API_KEY}&units=${process.env.WEATHER_API_TEMP_UNIT}`
+const baseUrl = process.env.WEATHER_API_BASE_URL
+const appId = process.env.WEATHER_API_KEY
+const unit = process.env.WEATHER_API_TEMP_UNIT
+const urlOptions = `&appid=${appId}&units=${unit}`
+
+const getBathCitiesUrl = ({cities}) => {
+  return `${baseUrl}/group?id=${cities}${urlOptions}`
 }
+
+const getCityDetailUrl = ({city}) => {
+  return `${baseUrl}/weather?q=${city}${urlOptions}`
+}
+
+const getCityDetailOneCallUrl = ({lat, lon}) => {
+  return `${baseUrl}/onecall?lat=${lat}&lon=${lon}${urlOptions}`
+}
+
+export {getBathCitiesUrl, getCityDetailUrl, getCityDetailOneCallUrl}
