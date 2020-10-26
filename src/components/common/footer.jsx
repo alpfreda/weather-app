84fdb5bf53a3d7a5285272b7635f9700880cd602
jsgeekes <jsgeekes@gmail.com>
2020-10-26 22:59:36 +0300

    Fix some bug

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => (
   <div className='footer'>
-    Developed in {new Date().getFullYear()} by <a href='#'>Mehdi Ak</a>
+    Developed in {new Date().getFullYear()} by <a href='https://github.com/jsgeekes' target="_blank">Mehdi Ak</a>
   </div>
 )
 
--- src/components/common/loader.jsx
+++ src/components/common/loader.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Loader = () => (
     <div className='cloud-wrapper'>
         <div className="cloud">
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="sun"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="sun"></div>
         </div>
     </div>
 )
