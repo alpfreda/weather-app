afae0d1cc7fbac21d549c9535bb9523083dbe41b
jsgeekes <jsgeekes@gmail.com>
2020-10-26 22:51:59 +0300

    Add footer

---
+++ src/components/common/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className='footer'>
+    Developed in {new Date().getFullYear()} by <a href='#'>Mehdi Ak</a>
+  </div>
+)
+
+export default Footer
--- src/layouts/public-layout.jsx
+++ src/layouts/public-layout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Routes from '../routes'
+import Footer from '../components/common/footer'
 
 const PublicLayout = () => {
-  return <div className='container'><Routes /></div>
+  return <div className='container'>
+    <Routes />
+    <Footer/>
+  </div>
 }
 
 export default PublicLayout
