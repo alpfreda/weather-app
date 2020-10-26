8186e11512e1c77482331b922e0c786e7a6670c5
jsgeekes <jsgeekes@gmail.com>
2020-10-26 22:52:15 +0300

    Fix responsive of detail page

--- src/components/weather/detail.jsx
+++ src/components/weather/detail.jsx
@@ -7,7 +7,7 @@ const Detail = ({ city }) => {
   const [{ weather, isLoading, isError }] = useWeatherApi(city, null)
 
   const defaultWeather = weather ? weather.current.weather[0] : null
-  return <> 
+  return <>
     {isError && <div>Something went wrong ...</div>}
     {isLoading ? <Loader /> :
       <>
@@ -15,15 +15,16 @@ const Detail = ({ city }) => {
           weather && <div className='weather'>
             <div className='weather-detail'>
               <div className='weather-detail-primary'>
+                <div className='weather-detail-primary-today'>Today</div>
                 <img src={`http://openweathermap.org/img/wn/${defaultWeather.icon}@4x.png`}
                   alt={defaultWeather.description} className="weather-detail-primary-img" />
                 <div className='weather-detail-primary-state'>{defaultWeather.description}</div>
               </div>
               <div className='weather-detail-info'>
                 <div className='weather-detail-info-temp'>{Math.round(weather.current.temp)}<sup>°C</sup></div>
-                <div className='weather-detail-info-temp'>{weather.current.wind_speed}</div>
-                <div className='weather-detail-info-temp'>{weather.current.pressure}</div>
-                <div className='weather-detail-info-temp'>{weather.current.humidity}</div>
+                <div className='weather-detail-info-other'><span>Wind</span>{weather.current.wind_speed}kw</div>
+                <div className='weather-detail-info-other'><span>Pressure</span>{weather.current.pressure}%</div>
+                <div className='weather-detail-info-other'><span>Humidity</span>{weather.current.humidity}%</div>
               </div>
               <div className='weather-detail-days'>
                 {
@@ -35,21 +36,14 @@ const Detail = ({ city }) => {
                         }
                       </div>
                       <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                        alt={day.weather[0].description} className="weather-detail-primary-img" />
-                      {/* <div className='weather-detail-days-item-info'>
-                        {day.temp.day}
-                      </div> */}
+                        alt={day.weather[0].description} className="weather-detail-days-item-img" />
                       <div className='weather-detail-days-item-info'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill='green' height="32" width="32" viewBox="0 0 31.999999 31.999996"><path d="m16.001 1029.4c-0.2706 0-0.50984 0.1016-0.70117 0.293l-2.9727 2.9687c-0.98171 0.9425 0.47156 2.3958 1.4141 1.4141l1.2598-1.252v9.5762c0 0.554 0.446 1 1 1s0.99883-0.446 0.99883-1v-9.5762l1.2598 1.252c0.9425 0.9817 2.3958-0.4716 1.4141-1.4141l-2.9727-2.9687c-0.19133-0.1914-0.42941-0.2971-0.70001-0.293z" transform="translate(0 -1020.4)" /></svg>
                         {Math.round(day.temp.min)}<sup>°C</sup>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill='red' height="32" width="32" viewBox="0 0 31.999999 31.999996"><path d="m15.999 1043.4c0.2706 0 0.50984-0.1016 0.70117-0.293l2.9727-2.9687c0.98171-0.9425-0.47156-2.3958-1.4141-1.4141l-1.2598 1.252v-9.5762c0-0.554-0.446-1-1-1s-0.99883 0.446-0.99883 1v9.5762l-1.2598-1.252c-0.9425-0.9817-2.3958 0.4716-1.4141 1.4141l2.9727 2.9687c0.19133 0.1914 0.42941 0.2971 0.70001 0.293z" transform="translate(0 -1020.4)" /></svg>
-                        {Math.round(day.temp.max)}<sup>°C</sup>
                       </div>
                       <div className='weather-detail-days-item-info'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill='green' height="32" width="32" viewBox="0 0 31.999999 31.999996"><path d="m16.001 1029.4c-0.2706 0-0.50984 0.1016-0.70117 0.293l-2.9727 2.9687c-0.98171 0.9425 0.47156 2.3958 1.4141 1.4141l1.2598-1.252v9.5762c0 0.554 0.446 1 1 1s0.99883-0.446 0.99883-1v-9.5762l1.2598 1.252c0.9425 0.9817 2.3958-0.4716 1.4141-1.4141l-2.9727-2.9687c-0.19133-0.1914-0.42941-0.2971-0.70001-0.293z" transform="translate(0 -1020.4)" /></svg>
-                        {Math.round(day.temp.day)}<sup>°C</sup>
                         <svg xmlns="http://www.w3.org/2000/svg" fill='red' height="32" width="32" viewBox="0 0 31.999999 31.999996"><path d="m15.999 1043.4c0.2706 0 0.50984-0.1016 0.70117-0.293l2.9727-2.9687c0.98171-0.9425-0.47156-2.3958-1.4141-1.4141l-1.2598 1.252v-9.5762c0-0.554-0.446-1-1-1s-0.99883 0.446-0.99883 1v9.5762l-1.2598-1.252c-0.9425-0.9817-2.3958 0.4716-1.4141 1.4141l2.9727 2.9687c0.19133 0.1914 0.42941 0.2971 0.70001 0.293z" transform="translate(0 -1020.4)" /></svg>
-                        {Math.round(day.temp.night)}<sup>°C</sup>
+                        {Math.round(day.temp.max)}<sup>°C</sup>
                       </div>
                     </div>
                   })
@@ -57,22 +51,25 @@ const Detail = ({ city }) => {
               </div>
             </div>
             <div className='weather-detail-body'>
-              {
-                weather.hourly.map(hour => {
-                  return <div key={hour.dt} className='weather-detail-body-item'>
-                    <div className='weather-detail-body-item-time'>
-                      {
-                        new moment(hour.dt * 1000).format('HH:MM')
-                      }
-                    </div>
-                    <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}
-                      alt={defaultWeather.description} className="weather-detail-body-item-img" />
-                    {/* <div className='weather-detail-body-content-item-name'>
+              <div className='weather-detail-body-title'>Hourly Report</div>
+              <div className='weather-detail-body-items'>
+                {
+                  weather.hourly.map(hour => {
+                    return <div key={hour.dt} className='weather-detail-body-item'>
+                      <div className='weather-detail-body-item-time'>
+                        {
+                          new moment(hour.dt * 1000).format('HH:MM')
+                        }
+                      </div>
+                      <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}
+                        alt={defaultWeather.description} className="weather-detail-body-item-img" />
+                      {/* <div className='weather-detail-body-content-item-name'>
                         {hour.weather[0].description}
                       </div> */}
-                  </div>
-                })
-              }
+                    </div>
+                  })
+                }
+              </div>
             </div>
           </div>
         }
